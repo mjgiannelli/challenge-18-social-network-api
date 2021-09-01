0a9e3f22a56af945b40a39947fe6cc553e6a269c
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-31 23:09:25 -0500

    add routes for user model

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -6,10 +6,19 @@ const {
     createUser,
     updateUser,
     deleteUser
-  } = require('../../controllers/user-controller');
+} = require('../../controllers/user-controller');
 
-  // set up the get all and post routes at api/users
-  router
-  .route('/')
-  .get(getAllUsers)
-  .post(createUser);
+// set up the get all and post routes at api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// set up the get, put and delete routes at api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
